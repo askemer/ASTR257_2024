1f87cc5befa07f308abde0030773b0fa1c3eef88
aditya-sengupta <aditya.sengupta@berkeley.edu>
2022-11-01 16:44:24 -0700

    analysis for neptune done

--- student_work/adityars/ao/neptune.py
+++ student_work/adityars/ao/neptune.py
@@ -8,6 +8,14 @@ from tqdm import tqdm, trange
 datapath = "../../../Project 3--Neptune AO/"
 getlims = ZScaleInterval().get_limits
 
+filternames = ["H", "J", "K"]
+
+get_idxs = {
+    "H" : [1, 2, 3, 4],
+    "J" : [0, 1, 2, 3],
+    "K" : [1, 2, 3, 4]
+}
+
 def mediandiv_and_zerocorr(x):
     x_norm = x / np.nanmedian(x)
     # x_norm[np.where(x_norm == 0)] = 1
@@ -48,30 +56,57 @@ def get_sciences():
     scinames = filter(lambda x: len(x) > 13, listdir(path.join(datapath, "science")))
     for f in scinames:
         sciences[f[8]].append(fits.open(path.join(datapath, "science", f))[0].data.astype(np.float64))
+    
+    for filtername in sciences:
+        sciences[filtername] = [x for (i, x) in enumerate(sciences[filtername]) if i in get_idxs[filtername]] 
+        # python how dare you make something so easy into an O(N^2) operation. I will never forgive you 
+
+    return sciences
+
+def divide_flats(sciences, flats):
+    with np.errstate(divide='ignore', invalid='ignore'):
+        for k in sciences:
+            for i in range(len(sciences[k])):
+                sciences[k][i] /= flats[k]
+                sciences[k][i][wherenan(sciences[k][i])] = 0
 
     return sciences
 
 
-yl, yh, xl, xh = 250, 1250, 600, 1600
-yc = (yl + yh) / 2 
-xc = (xl + xh) / 2
-r = xc - xl
-xmesh, ymesh = np.meshgrid(np.arange(xl, xh), np.arange(yl, yh))
-mask = (xmesh - xc) ** 2 + (ymesh - yc) ** 2 < r ** 2
-
-def crop_to_ao(img):
-    img = img[yl:yh, xl:xh]
-    return img * mask
+yl, yh, xl, xh = 500, 1000, 950, 1450
 
 argmax = lambda x: np.where(x == np.max(x))
 
-def renorm_to_find_neptune(img):
-    img = n.crop_to_ao(img)
-    cmin, cmax = n.getlims(img)
+def renorm(img):
+    cmin, cmax = getlims(img)
     return np.nan_to_num(
         np.maximum(
             cmin, np.minimum(
                 cmax, img
             )
         )
-    )[200:-200, 200:-200]
+    )
+
+def renorm_and_crop_all(sciences, do_renorm=True):
+    if sciences["H"][0].shape[0] == 2048:
+        for f in ["H", "J", "K"]:
+            for i in range(4):
+                img = sciences[f][i][yl:yh, xl:xh]
+                if do_renorm:
+                    img = renorm(img)
+                sciences[f][i] = img
+    
+    return sciences
+
+def plot_all_images(sciences, filtername):
+    f, axs = plt.subplot_mosaic("01;23")
+    for i in range(4):
+        img = sciences[filtername][i]
+        ax = axs[str(i)]
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.imshow(img, cmap="gray")
+        #view_as_ds9(img, ax, cmap="gray")
+
+    f.suptitle(f"Dithered images of Neptune, {filtername} filter")
+
